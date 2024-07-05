865f09e3a76a78d28568af1163b3ee437ca00d24
yologrey <20241069@buaa.edu.cn>
2024-07-05 15:41:00 +0800

    feat: 完成静态页面

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import( '@/views/HomeView.vue' )
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/function',
+    name: 'function',
+    component: () => import( '@/views/FunctionVue.vue' )
+  },
+  {
+    path: '/resource',
+    name: 'resource',
+    component: () => import( '@/views/ResourceView.vue' )
+  },
+  {
+    path: '/result',
+    name: 'result',
+    component: () => import( '@/views/ResultView.vue' )
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
